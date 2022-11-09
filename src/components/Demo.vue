46e423dd92a71a0211f809867fdff73bf4e12085
smartvain <zlc.blue15210@gmail.com>
2022-11-09 11:11:16 +0900

    add: Demo.vue logic

---
+++ src/components/Demo.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useVirtualList } from '@vueuse/core'
+
+const data = ref(Array.from(Array(50).keys(), () => 'Lorem ipsum'))
+
+const { list, containerProps, wrapperProps } = useVirtualList(data, {
+  itemHeight: 96
+})
+</script>
+
+<template>
+  <div v-bind="containerProps" class="h-screen p-2 rounded">
+    <div v-bind="wrapperProps" class="max-w-sm mx-auto">
+      <div
+        v-for="{ index, data } in list"
+        :key="index"
+        class="rounded-lg h-[80px] flex flex-col px-4 justify-center bg-neutral-800 mb-4 border-neutral-600"
+      >
+        <h2 class="mb-2 text-2xl">Item ${{ index }}</h2>
+        <p class="text-sm">{{ data }}</p>
+      </div>
+    </div>
+  </div>
+</template>
